3cba36465d5d74bf49e011ed2e398317011deaa3
Franek <frrenk.p@gmail.com>
2019-10-22 15:46:03 +0200

    added further error customization for POSTing an invalid object

--- src/main/java/pl/piaseckif/ppmtool/web/ProjectController.java
+++ src/main/java/pl/piaseckif/ppmtool/web/ProjectController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,9 @@ import pl.piaseckif.ppmtool.domain.Project;
 import pl.piaseckif.ppmtool.services.ProjectService;
 
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
@@ -24,7 +28,13 @@ public class ProjectController {
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
 
         if (result.hasErrors()) {
-            return new ResponseEntity<String>("Invalid Project Object", HttpStatus.BAD_REQUEST);
+            List<FieldError> fieldErrors = result.getFieldErrors();
+            Map<String, String> errorMap = new HashMap<>();
+            for (FieldError e :
+                    fieldErrors) {
+                errorMap.put(e.getField(), e.getDefaultMessage());
+            }
+            return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
         }
 
         Project project1 = projectService.saveOrUpdateProject(project);
